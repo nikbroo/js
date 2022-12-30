2b56e994a6bba454de9348c237196635e530501e
nikbroo <nikhilng.1997@gmil.com>
2022-12-30 12:42:58 +0530

    first_commit

---
+++ demo.js
@@ -0,0 +1,17 @@
+// let va = 545;
+function va(){
+    return 123
+}
+
+let str1 = `abc1 ${va()}`;
+let str2 = "abc2";
+let str3 = "abc3";
+
+// console.log(str1);
+// let str = '';
+// for(let i = 0; i < str1.length; i++){
+//     str += str1[i];
+// }
+// console.log(str[1]);
+// console.log(parseInt("10"))
+let num = 5;
